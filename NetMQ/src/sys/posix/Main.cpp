a4f1f0304f39b2c3f8fd8bcbc93e270e41511f71
Marco Grimaldi <marco.grimaldi20@outlook.com>
2025-08-05 22:22:12 +1000

    Add project files.

---
+++ NetMQ/src/sys/posix/Main.cpp
@@ -0,0 +1,12 @@
+int main(int argc, char **argv)
+{
+	// initialize and configure the server
+
+	// loop through the message queue and handle messages/commands
+
+	// the client can publish to a topic or subscribe to a topic, or both
+
+	// server will provide a few commands so far: conn, pub, sub, unsub, exit
+
+	return 0;
+}
